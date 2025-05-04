ee0d689dcaaac8108207d9f4272323cdc4e81d4b
Mateusz Świerczek <omatix2003@interia.pl>
2025-05-04 17:52:49 +0200

    event link to customer

--- src/pages/Events/EventEdit.jsx
+++ src/pages/Events/EventEdit.jsx
@@ -31,7 +31,6 @@ function CustomerEdit() {
       >
         {(RecordContext, record) => (
           <GlContainer>
-            {JSON.stringify(record)}
             <GlRow>
               <GlButton
                 className="primary"
@@ -69,15 +68,42 @@ function CustomerEdit() {
 
             <GlEdit field="date" Context={RecordContext} />
 
-            <GlLookup
-              dataSetIdent="glCustomersAll"
-              nameSpace="crm"
-              Context={RecordContext}
-              field={"customer"}
-              fieldInLookup={"ident"}
-            >
-              {(row) => <div>{JSON.stringify(row)}</div>}
-            </GlLookup>
+            <GlRow>
+              <GlLookup
+                dataSetIdent="glCustomersAll"
+                nameSpace="crm"
+                Context={RecordContext}
+                field={"customer"}
+                fieldInLookup={"ident"}
+              >
+                {(row) => <div>{JSON.stringify(row)}</div>}
+              </GlLookup>
+              <GlButton
+                className="primary"
+                style={{ height: "34px", marginTop: "auto" }}
+                action={() => {
+                  navigate(`/customers/${record.customer}`);
+                }}
+              >
+                <svg
+                  className="nav-icon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                  <circle cx="9" cy="7" r="4"></circle>
+                  <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                  <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                </svg>
+              </GlButton>
+            </GlRow>
             <GlEdit field="customer" Context={RecordContext} />
 
             <GlEdit field="type" Context={RecordContext} />
